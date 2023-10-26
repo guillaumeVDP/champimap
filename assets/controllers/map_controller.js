4b29a2d83e2e0e2037517a11cd50ec1f8a9d3d23
Guillaume VDP <vdp.guillaume@gmail.com>
2023-10-26 12:12:44 +0200

    Showing he user on the map

--- assets/controllers/map_controller.js
+++ assets/controllers/map_controller.js
@@ -12,21 +12,32 @@ export default class extends Controller {
     SHOW_LANDMARK_TAG = "show_landmark";
 
     popup = L.popup();
+    map = null;
     latlng = null;
+    userMarker = null;
 
     connect() {
-        // Map init
-        let map = L.map('map', {
-            center: [43.5495051, 2.7590491],
-            zoom: 20
+        /********************
+         ***** Map init *****
+         ********************/
+        this.map = L.map('map', {
+            // center: [43.5495051, 2.7590491],
+            // zoom: 20
         });
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-        map.on('click', this.onMapClick.bind(this));
+        }).addTo(this.map);
+        this.map.on('click', this.onMapClick.bind(this));
 
-        // Markers init
+        /**********************
+         **** Markers init ****
+         **********************/
+        // User
+        // TODO: fallback when user don't give pos, set map center to Valière [43.5495051, 2.7590491]
+        navigator.geolocation.getCurrentPosition(this.showUserLocation.bind(this));
+
+        // Findings
         const iconRetinaUrl = 'images/marker-icon-2x.png';
         const shadowUrl = 'images/marker-shadow.png';
         let iconUrl = 'images/marker-icon2.png';
@@ -40,6 +51,16 @@ export default class extends Controller {
             tooltipAnchor: [16, -28],
             shadowSize: [41, 41]
         });
+
+        this.findingsValue.forEach((finding) => {
+            let latlng = [finding.location.latitude, finding.location.longitude];
+            L.marker(latlng)
+                .addTo(this.map)
+                .setIcon(iconMushroom)
+                .on('click', this.dispatchFct.bind(this, this.SHOW_MUSHROOM_TAG, finding.id))
+        });
+
+        // Landmarks
         iconUrl = 'images/marker-icon-landmark.png';
         let iconLandmark = L.icon({
             iconRetinaUrl,
@@ -52,23 +73,17 @@ export default class extends Controller {
             shadowSize: [41, 41]
         });
 
-        this.findingsValue.forEach((finding) => {
-            let latlng = [finding.location.latitude, finding.location.longitude];
-            L.marker(latlng)
-                .addTo(map)
-                .setIcon(iconMushroom)
-                .on('click', this.dispatchFct.bind(this, this.SHOW_MUSHROOM_TAG, finding.id))
-        });
-
         this.landmarksValue.forEach((landmark) => {
             let latlng = [landmark.location.latitude, landmark.location.longitude];
             L.marker(latlng)
-                .addTo(map)
+                .addTo(this.map)
                 .setIcon(iconLandmark)
                 .on('click', this.dispatchFct.bind(this, this.SHOW_LANDMARK_TAG, landmark.id))
         });
 
-        // Popup init
+        /********************
+         **** popup init ****
+         ********************/
         let newMushroomBtn = document.createElement('button');
         newMushroomBtn.innerHTML = "🍄";
         newMushroomBtn.addEventListener('click', this.dispatchFct.bind(this, this.ADD_MUSHROOM_TAG));
@@ -79,6 +94,33 @@ export default class extends Controller {
         container.appendChild(newMushroomBtn);
         container.appendChild(newLandmarkBtn);
         this.popup.setContent(container);
+    }
+
+    showUserLocation(position) {
+        const iconRetinaUrl = 'images/marker-icon-2x.png';
+        const shadowUrl = 'images/marker-shadow.png';
+        let iconUrl = 'images/marker-icon.png';
+        let iconUser = L.icon({
+            iconRetinaUrl,
+            iconUrl,
+            shadowUrl,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            tooltipAnchor: [16, -28],
+            shadowSize: [41, 41]
+        });
+        this.userMarker = L.marker([position.coords.latitude, position.coords.longitude]);
+        this.userMarker.setIcon(iconUser).addTo(this.map);
+        console.log("Pan to user");
+        console.log(this.map);
+        this.map.setView(new L.LatLng(position.coords.latitude, position.coords.longitude), 20);
+        navigator.geolocation.watchPosition(this.trackUser.bind(this));
+    }
+
+    trackUser(position) {
+        console.log("User position updated");
+        this.userMarker.setLatLng([position.coords.latitude, position.coords.longitude]);
     }
 
     onMapClick(event) {
